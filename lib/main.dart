d48101d3b8a6c253f2db699624e0daab83f17233
pauloRog <paulo-rpf@hotmail.com>
2020-05-02 20:14:33 -0400

    Structuring project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app_restaurant/splash.screen.dart';
+import 'package:app_restaurant/view/splash.screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
